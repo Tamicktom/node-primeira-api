423066611b2cdfdce4256fd3f60666319d307999
Tamicktom <rickpokemon38@gmail.com>
2025-08-23 18:14:46 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const fastify = require('fastify');
+
+const app = fastify();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running on ${address}`);
+});
