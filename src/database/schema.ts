05d0970558e1643889cc5c5bd6a2be604f3aaa3f
Tamicktom <rickpokemon38@gmail.com>
2025-08-30 20:56:36 -0300

    Refactor database schema definition for users, courses, and enrollments tables to improve readability and maintainability; added password field to users table.

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,39 +1,42 @@
 //* Libraries imports
-import {
-  pgTable,
-  uuid,
-  text,
-  integer,
-  date,
-  primaryKey,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, date, primaryKey, uniqueIndex } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: uuid().primaryKey().defaultRandom(),
-  name: text().notNull(),
-  email: text().notNull().unique(),
+export const usersTable = pgTable(
+  "users",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    name: text().notNull(),
+    email: text().notNull().unique(),
+    password: text().notNull(),
 
-  createdAt: date().notNull().defaultNow(),
-  updatedAt: date().notNull().defaultNow(),
-});
+    createdAt: date().notNull().defaultNow(),
+    updatedAt: date().notNull().defaultNow(),
+  }
+);
 
-export const coursesTable = pgTable("courses", {
-  id: uuid().primaryKey().defaultRandom(),
-  title: text().notNull(),
-  description: text().notNull(),
+export const coursesTable = pgTable(
+  "courses",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    title: text().notNull(),
+    description: text().notNull(),
 
-  createdAt: date().notNull().defaultNow(),
-  updatedAt: date().notNull().defaultNow(),
-});
+    createdAt: date().notNull().defaultNow(),
+    updatedAt: date().notNull().defaultNow(),
+  }
+);
 
-export const enrollmentsTable = pgTable("enrollments", {
-  userId: uuid().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
-  courseId: uuid().notNull().references(() => coursesTable.id, { onDelete: "cascade" }),
+export const enrollmentsTable = pgTable(
+  "enrollments",
+  {
+    userId: uuid().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
+    courseId: uuid().notNull().references(() => coursesTable.id, { onDelete: "cascade" }),
 
-  createdAt: date().notNull().defaultNow(),
-  updatedAt: date().notNull().defaultNow(),
-}, (table) => [
-  primaryKey({ columns: [table.userId, table.courseId] }),
-  uniqueIndex("enrollments_unique_index").on(table.userId, table.courseId),
-]);
+    createdAt: date().notNull().defaultNow(),
+    updatedAt: date().notNull().defaultNow(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.userId, table.courseId] }),
+    uniqueIndex("enrollments_unique_index").on(table.userId, table.courseId),
+  ]
+);
