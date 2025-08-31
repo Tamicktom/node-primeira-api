df56931b2869e9263ade8a27d0d436fe6bc565e6
Tamicktom <rickpokemon38@gmail.com>
2025-08-31 15:58:56 -0300

    Update server to listen on all network interfaces by specifying host as "0.0.0.0".

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 //* Local imports
 import { app } from "./app.ts";
 
-app.listen({ port: 3333 }, (err, address) => {
+app.listen({ port: 3333, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
