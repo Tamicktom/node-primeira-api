edf52de7ca79df97e971074b99f2c7a2dc4c6ce7
Tamicktom <rickpokemon38@gmail.com>
2025-08-31 15:32:51 -0300

    Add user role assignment and create utility function for authenticated user generation with JWT token. Enhance user creation in tests to include role and password management.

--- src/tests/factories/make-user.ts
+++ src/tests/factories/make-user.ts
@@ -1,5 +1,6 @@
 //* Libraries imports
 import { fakerPT_BR as faker } from "@faker-js/faker";
+import jwt from "jsonwebtoken";
 import { hash } from "argon2";
 
 //* Local imports
@@ -23,6 +24,7 @@ export async function makeUsers(amount: number = 1) {
             name: faker.person.fullName(),
             email: faker.internet.email(),
             password: await hash(passwordBeforeHash),
+            role: "student",
           })
           .returning({
             id: usersTable.id,
@@ -47,4 +49,44 @@ export async function makeUsers(amount: number = 1) {
 
 export async function makeUser() {
   return (await makeUsers(1))[0];
+}
+
+/**
+ * Make an authenticated user and return the user with the original password
+ */
+
+export async function makeAuthenticatedUser(role: "student" | "admin") {
+  const passwordBeforeHash = faker.internet.password();
+
+  const [user] = await db
+    .insert(usersTable)
+    .values({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: await hash(passwordBeforeHash),
+      role,
+    })
+    .returning({
+      id: usersTable.id,
+      name: usersTable.name,
+      email: usersTable.email,
+      role: usersTable.role,
+      password: usersTable.password,
+    });
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET!,
+  );
+
+  const newUser = {
+    ...user,
+    passwordBeforeHash,
+    token,
+  }
+
+  return newUser;
 }
